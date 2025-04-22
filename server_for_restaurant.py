c4033319a7cb30f9f6977a46bfc30de7a340a089
francis Valenzuela Ganapin <66492676+francisganapin@users.noreply.github.com>
2025-04-22 23:54:42 +0800

    4/22/2025

--- server_for_restaurant.py
+++ server_for_restaurant.py
@@ -1,4 +1,8 @@
+<<<<<<< HEAD
 from flask import Flask, render_template, request
+=======
+from flask import Flask, render_template, request,redirect,url_for,flash,get_flashed_messages
+>>>>>>> a6e1b0b (4/22/2025)
 import webbrowser
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
@@ -17,7 +21,11 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Avoids a warning
 
 db = SQLAlchemy(app)
 
+<<<<<<< HEAD
 class Booking_Tb(db.Model):
+=======
+class BookingTb(db.Model):
+>>>>>>> a6e1b0b (4/22/2025)
     id = db.Column(db.Integer, primary_key=True)
     id_card = db.Column(db.String(27),nullable=False)
     select_date = db.Column(db.String(20))
@@ -29,7 +37,11 @@ class Booking_Tb(db.Model):
 
 
 
+<<<<<<< HEAD
 class Contact_Tb(db.Model):
+=======
+class ContactTb(db.Model):
+>>>>>>> a6e1b0b (4/22/2025)
     id = db.Column(db.Integer,primary_key=True)
     first_name = db.Column(db.String(20),nullable=False)
     last_name = db.Column(db.String(20),nullable=False)
@@ -41,6 +53,7 @@ class Contact_Tb(db.Model):
 
     def __repr__(self):
 <<<<<<< HEAD
+<<<<<<< HEAD
         return f'<Contact {self.name}>'
 
 class Food_stock(db.Model):
@@ -51,11 +64,21 @@ class Stock_Tb(db.Model):
 >>>>>>> 1ccf7231c96113956d0a849f97e9948cc4ab5cec
     id = db.Column(db.Integer,primary_key=True)
     name = db.Column(db.String(25),nullable=False)
+=======
+        return f'<Contact {self.first_name},{self.last_name}>'
+
+class Food_stock(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(25),nullable=False,unique=True)
+>>>>>>> a6e1b0b (4/22/2025)
     category = db.Column(db.String(25),nullable=False)
     quantity = db.Column(db.Integer,nullable=False)
     price = db.Column(db.Integer,nullable=False)
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> a6e1b0b (4/22/2025)
 class Customer_list(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     card_id = db.Column(db.String(25),nullable=False)
@@ -69,6 +92,7 @@ class Customer_list(db.Model):
     #product = db.Column(db.String(100),nullable=False)
     #amount = db.Column(db.Float,nullable=False)
     #order_date = db.Column(db.DateTime,default=datetime.utcnow)
+<<<<<<< HEAD
 =======
 class Menu_Tb(db.Model):
     id = db.Column(db.Integer,primary_key=True)
@@ -91,6 +115,8 @@ class Order(db.Model):
     amount = db.Column(db.Float,nullable=False)
     order_date = db.Column(db.String(100),nullable=False)
 >>>>>>> 1ccf7231c96113956d0a849f97e9948cc4ab5cec
+=======
+>>>>>>> a6e1b0b (4/22/2025)
 
 # Ensure the database is created
 with app.app_context():
@@ -99,6 +125,9 @@ with app.app_context():
 
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> a6e1b0b (4/22/2025)
 @app.route('/register/',methods=['GET','POST'])
 def register_customer():
     
@@ -111,13 +140,21 @@ def register_customer():
 
 @app.route('/food_stock/',methods=['GET','POST'])
 def food_list():
+<<<<<<< HEAD
     stock_list_data = Food_stock.query
+=======
+
+    page = request.args.get('page',1,type=int)
+
+    query = Food_stock.query
+>>>>>>> a6e1b0b (4/22/2025)
 
     if request.method == 'POST':
         query_name = request.form.get('query_name', '').strip()
         query_category = request.form.get('query_category', '').strip()
 
         if  query_name:
+<<<<<<< HEAD
             stock_list_data = stock_list_data.filter(Food_stock.name.ilike(f'%{query_name}%'))
 
        
@@ -183,6 +220,101 @@ def booking_list():
                 contact_list_data = [ data for data in contact_list_data if query_occasion in data.occasion]
 >>>>>>> 1ccf7231c96113956d0a849f97e9948cc4ab5cec
 
+=======
+            query = query.filter(Food_stock.name.ilike(f'%{query_name}%'))
+
+       
+        if query_category and query_category != 'All':
+            query = query.filter_by(category=query_category)
+
+    stock_list_data = query.paginate(page=page,per_page=6,error_out=False)
+
+    return render_template('owner_template/food_list.html',stock_list=stock_list_data)
+
+# add front end message handler  4/9/2025
+@app.route('/food_stock/add/',methods=['GET','POST'])
+def add_food_stock():
+    message = ''
+    if request.method == 'POST':
+        try:
+            name = request.form.get('food_name')
+            category = request.form.get('food_category')
+            quantity = request.form.get('food_quantity')
+            price = request.form.get('food_price')
+
+            print(name,category,quantity,price)
+
+            new_food = Food_stock(name=name,
+                                category=category,
+                                quantity=quantity,
+                                price=price)
+            print(new_food)
+            db.session.add(new_food)
+            db.session.commit()
+            flash('food was added succesfuly')
+        except Exception as e:
+            flash('sorry duplicate Name for Foods')
+            print(f'name was duplicate sorry {e}')
+
+    return redirect(url_for('food_list',message=message))
+
+
+@app.route('/food_stock/upate/<int:id>',methods=['GET','POST'])
+def update_food_stock(id):
+    message = ''
+    food = Food_stock.query.get_or_404(id)
+    if request.method == 'POST':
+        try:
+            quantity = request.form.get('food_quantity')
+            price = request.form.get('food_price')
+
+        
+            food.quantity = quantity
+            food.price = price
+           
+            db.session.commit()
+            flash('Food was updated successfuly')
+        except Exception as e:
+            db.session.rollback()
+            flash('sorry duplicate Name for Foods')
+            print(f'name was duplicate sorry{e}')
+    return redirect(url_for('food_list',message=message))
+
+@app.route('/contact_list/', methods=['GET', 'POST'])
+def booking_list():
+    
+    # set the page where we at
+    page = request.args.get('page',1,type=int) 
+    
+    query = ContactTb.query
+
+    if request.method == 'POST':
+        query_name = request.form.get('query_name')
+        query_email = request.form.get('query_email')
+        query_occasion = request.form.get('query_occasion')
+
+        print(query_name)
+        result = query.all()
+        print(result)
+
+        if query_name:
+            query = query.filter(
+                db.or_(ContactTb.first_name.ilike(f'%{query_name}%'),
+                ContactTb.last_name.ilike(f'%{query_name}%')
+                ))
+                
+        if query_email:
+            query = query.filter(
+                ContactTb.email.ilike(f'%{query_email}%')
+                )
+
+        if query_occasion and query_occasion != 'All':
+            contact_list_data = query.filter_by(occasion=query_occasion)
+    
+    contact_list_data = query.paginate(page=page,per_page=6,error_out=False)
+    
+    print(contact_list_data)
+>>>>>>> a6e1b0b (4/22/2025)
     return render_template('owner_template/contact_list.html',contact_list_data=contact_list_data)
 
 
