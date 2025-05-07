d4ed83f7b82e2a9fceb723c6fce6e12c4ff43fd5
francis Valenzuela Ganapin <66492676+francisganapin@users.noreply.github.com>
2025-05-08 03:21:43 +0800

    update css may 5 2025

--- server_for_customer.py
+++ server_for_customer.py
@@ -114,6 +114,6 @@ def blog():
     return render_template('blog.html')
 
 if __name__ == '__main__':
-    webbrowser.open("http://127.0.0.1:5000/home/")
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    webbrowser.open("http://127.0.0.1:5012/home/")
+    app.run(host='0.0.0.0', port=5012, debug=True)
 
--- server_for_restaurant.py
+++ server_for_restaurant.py
@@ -227,6 +227,11 @@ def booking_list_archive():
     return redirect(url_for('booking_list'))
 
 
+@app.route
+
+
+
+
 
 if __name__ == '__main__':
     webbrowser.open("http://127.0.0.1:5000/contact_list/")
